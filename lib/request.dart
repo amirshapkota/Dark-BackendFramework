c47e8336f9d87c357c6e90f71024349eb4c6cd3e
Apurba Adhikari <apurba4790@gmail.com>
2022-09-01 17:03:04 +0545

    A file for request created The new request object will be able to validate and get parameters and data

---
+++ lib/request.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+class Request {
+
+  HttpRequest? httpRequest;
+
+  Request(this.httpRequest);
+
+  
+  void validate(Map rules)
+  {
+
+  }
+
+
+  Map getData()
+  {
+    return {};
+  }
+
+  Map getParams()
+  {
+    return {};
+  }
+
+
+
+}
