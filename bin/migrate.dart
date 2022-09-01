845f2e7fd4ca408dd702d1e8934b6c6dad727369
Apurba Adhikari <apurba4790@gmail.com>
2022-09-01 14:28:29 +0545

    Message added in migration

--- bin/migrate.dart
+++ bin/migrate.dart
@@ -4,6 +4,8 @@ void main(List<String> args) {
   
   for (var model in models)
   {
+    String name = model.name;
+    print("[ Migration ] Migrating $name");
     if (args.length != 0 && args[0] == "--force") 
     {
       model.migrate(force: true);
