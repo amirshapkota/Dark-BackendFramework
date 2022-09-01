4d625614915e7a437f5fb612e8a3a72c13a3da1c
Apurba Adhikari <apurba4790@gmail.com>
2022-09-01 17:01:07 +0545

    Configuration Added for The Database and JWT

---
+++ config.dart
@@ -0,0 +1,9 @@
+
+// Database Settings
+final String connection = "sqlite";
+final String database = "database.db";
+final String username = "";
+final String password = "";
+
+// JWT Password
+final String JWTPassKey = "";
